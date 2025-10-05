e010334e871c17dbd1cc514349a469877090e960
Gerald Clark <geraldclarkaudio@gmail.com>
2025-10-04 22:14:48 -0500

    impressmentt done. Level select button set up

--- Assets/PaperKiteStudio/Scripts/Impressment/ImpressWinZone.cs
+++ Assets/PaperKiteStudio/Scripts/Impressment/ImpressWinZone.cs
@@ -5,12 +5,24 @@ namespace PaperKiteStudio.Dangers
 {
     public class ImpressWinZone : MonoBehaviour
     {
+        [SerializeField]
+        private GamePhaseManager gamePhaseManager;
+        [SerializeField]
+        private GameObject _endImpressmentCutscene;
+
+        private void Start()
+        {
+            gamePhaseManager = FindAnyObjectByType<GamePhaseManager>();
+        }
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if (collision.CompareTag("Player"))
             {
-                //win impressment level
-                //automatically start a cutscene that shows the player entering the house and fading out. 
+                if(gamePhaseManager.GetGamePhase() == 6) // if we havent beat this yet.. 
+                {
+                    gamePhaseManager.IncrementGamePhase();
+                }
+                _endImpressmentCutscene.SetActive(true);
             }
         }
     }
